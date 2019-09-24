7401ce37809ff9ed581af555e078e4ebcfbcb261
Pedro Marchante <pmarc560@gmail.com>
2019-09-24 06:19:48 -0400

    forms take input

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,43 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import IUsers from './models/Users';
 import agent from './api/agent';
-import {
-  Container,
-  Segment,
-  Grid,
-  Form,
-  Checkbox,
-  Button
-} from 'semantic-ui-react';
+import { Segment, Grid, Form, Button } from 'semantic-ui-react';
 import UserList from './UserList';
 
 const App: React.FC = () => {
   const [users, setUsers] = useState<IUsers[]>([]);
+
+  // const createUser = ()=>{
+  //   let newUser ={
+  //     ...
+  //   }
+  //   agent.create(newUser).then(()=>{
+
+  //     setUsers([...users, newUser])
+  //   })
+  // }
 
   useEffect(() => {
     agent.list().then(response => {
       setUsers(response);
     });
   }, []);
+  const handleInputChange = (event: FormEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
 
+    //setUsers({ ...users, [name]: value });
+  };
   return (
     <Grid centered>
       <Grid.Column width='5'>
-        <Segment style={{ marginTop: 250 }}>
+        <Segment style={{ marginTop: 250 }} clearing>
           <Form>
-            <Form.Field>
-              <label>User Name</label>
-              <input placeholder='User Name' />
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <input placeholder='Password' type='password' />
-            </Form.Field>
-            <Button type='submit'>Submit</Button>
+            <Form.Input
+              name='userName'
+              placeholder='Username'
+              onChange={handleInputChange}
+            />
+
+            <Form.Input
+              name='password'
+              placeholder='Password'
+              onChange={handleInputChange}
+            />
+
+            <Button type='submit' positive content='Submit' floated='right' />
+            <Button negative content='Clear form' floated='right' />
           </Form>
         </Segment>
       </Grid.Column>
-
+      <Grid.Column width='2' />
       <Grid.Column width='5'>
         <UserList users={users} />
       </Grid.Column>
